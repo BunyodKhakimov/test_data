ed3c67a8ce5bb5cc0e981964cb7bf742b5bd3670
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-05-08 16:50:36 +0500

    test commit

---
+++ app/Form.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Form extends Model
+{
+    use SoftDeletes;
+    protected $guarded = [];
+}
---
+++ database/migrations/2022_05_08_113132_create_fields_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fields', function (Blueprint $table) {
+            $table->id();
+            $table->string('form_uid');
+            $table->string('title');
+            $table->text('description');
+            $table->string('type');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('form_uid')->on('forms')->references('uid')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fields');
+    }
+}
